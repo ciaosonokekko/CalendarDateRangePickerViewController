e1afb9fe2faa44677884aea89a9d81d76a81db2a
Francesco Cosenza <francesco.cosenza@dualb.it>
2021-01-29 10:31:08 +0100

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "CalendarDateRangePickerViewController",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CalendarDateRangePickerViewController",
-            targets: ["CalendarDateRangePickerViewController"]),
+            targets: ["CalendarDateRangePickerViewController"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CalendarDateRangePickerViewController",
-            dependencies: []),
-        .testTarget(
-            name: "CalendarDateRangePickerViewControllerTests",
-            dependencies: ["CalendarDateRangePickerViewController"]),
+            path: "CalendarDateRangePickerViewController/Classes"
+        )
     ]
 )
